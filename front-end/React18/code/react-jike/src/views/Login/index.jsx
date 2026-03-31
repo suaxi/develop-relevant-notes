ea84ada6811f8b0a107e32db5ce773675b1604c4
suaxi <281463547@qq.com>
2026-03-31 21:28:00 +0800

    React：jike-demo：登录表单、基础校验

--- front-end/React18/code/react-jike/src/views/Login/index.jsx
+++ front-end/React18/code/react-jike/src/views/Login/index.jsx
@@ -1,5 +1,51 @@
-function Login() {
-  return <h1>Login</h1>
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
+  const onFinish = (val) => {
+    console.log(val)
+  }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号'
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号'
+              }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码'
+              }
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
