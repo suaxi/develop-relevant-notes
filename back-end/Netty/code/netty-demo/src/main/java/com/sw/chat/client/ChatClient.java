38b7974a85626fca74b651707e8020e2e4f9b759
suaxi <281463547@qq.com>
2026-07-19 23:48:21 +0800

    Netty：chat-demo 登录之后的操作（线程间通信）

--- back-end/Netty/code/netty-demo/src/main/java/com/sw/chat/client/ChatClient.java
+++ back-end/Netty/code/netty-demo/src/main/java/com/sw/chat/client/ChatClient.java
@@ -1,6 +1,7 @@
 package com.sw.chat.client;
 
 import com.sw.chat.message.LoginRequestMessage;
+import com.sw.chat.message.LoginResponseMessage;
 import com.sw.chat.protocol.MessageCodecSharable;
 import com.sw.chat.protocol.ProcotolFrameDecoder;
 import io.netty.bootstrap.Bootstrap;
@@ -16,6 +17,8 @@ import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author suaxi
@@ -27,6 +30,8 @@ public class ChatClient {
         NioEventLoopGroup group = new NioEventLoopGroup();
         LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        CountDownLatch WAIT_FOR_LOGIN = new CountDownLatch(1);
+        AtomicBoolean LOGIN = new AtomicBoolean(false);
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.channel(NioSocketChannel.class);
@@ -38,12 +43,18 @@ public class ChatClient {
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
                     ch.pipeline().addLast("chat client", new ChannelInboundHandlerAdapter() {
-                        //接收响应消息
-
-
+                        // 接收响应消息
                         @Override
                         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                             log.debug("resp msg: {}", msg);
+                            if (msg instanceof LoginResponseMessage) {
+                                LoginResponseMessage loginResponseMessage = (LoginResponseMessage) msg;
+                                if (loginResponseMessage.isSuccess()) {
+                                    LOGIN.set(true);
+                                }
+                                // 唤醒登录之后的操作
+                                WAIT_FOR_LOGIN.countDown();
+                            }
                         }
 
                         // 连接建立后触发 active 事件
@@ -60,6 +71,28 @@ public class ChatClient {
                                 // 构建登录消息对象
                                 LoginRequestMessage loginRequestMessage = new LoginRequestMessage(username, password);
                                 ctx.writeAndFlush(loginRequestMessage);
+                                try {
+                                    WAIT_FOR_LOGIN.await();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                                if (!LOGIN.get()) {
+                                    ctx.channel().close();
+                                    return;
+                                }
+
+                                while (true) {
+                                    System.out.println("==================================");
+                                    System.out.println("send [username] [content]");
+                                    System.out.println("gsend [group name] [content]");
+                                    System.out.println("gcreate [group name] [m1,m2,m3...]");
+                                    System.out.println("gmembers [group name]");
+                                    System.out.println("gjoin [group name]");
+                                    System.out.println("gquit [group name]");
+                                    System.out.println("quit");
+                                    System.out.println("==================================");
+                                }
                             }, "system.in").start();
                         }
                     });
